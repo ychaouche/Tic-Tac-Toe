9998695ec54d2f858fb7873be4ca8df87d8be7b3
Jose Soto <luissotojose@gmail.com>
2013-06-28 15:50:23 -0400

    Add helper functions for AI

--- Plyr_Comp_options.py
+++ Plyr_Comp_options.py
@@ -3,3 +3,22 @@
 def isSpaceFree(grid, idx):
     # return true if the square is empty
     return grid[idx] == ""
+
+def chooseRandomMoveFromList(grid, movesList):
+    # Returns a valid move from the passed list on the passed board
+    # Returns None if there is no valid move
+    possibleMoves = []
+    for move in movesList:
+        if isSpaceFree(grid, move):
+            possibleMoves.append(move)
+
+    if len(possibleMoves) != 0:
+        return random.choice(possibleMoves)
+    else:
+        None
+
+# helps computer make his moves
+def makeMove(grid, letter, square):
+    # places letter on the grid
+    grid[square] = letter
+
