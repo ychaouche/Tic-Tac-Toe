7740ce8e8df6f0d4b201c3b65cb19b4a07fb649a
Jose Soto <luissotojose@gmail.com>
2013-06-28 19:50:37 -0400

    change is_winner from possible_moves to window_handler

--- possible_moves.py
+++ possible_moves.py
@@ -22,15 +22,3 @@ def makeMove(grid, letter, square):
     # places letter on the grid
     grid[square] = letter
 
-# checks for a winner
-def is_winner(gr, le):
-    # gr == grid and le == letter, returns True if there is a winner
-
-    return ((gr[0] == le and gr[1] == le and gr[2] == le) or # up the left side
-            (gr[0] == le and gr[4] == le and gr[8] == le) or # top left to bottom right diagonally
-            (gr[0] == le and gr[3] == le and gr[6] == le) or # top row
-            (gr[1] == le and gr[4] == le and gr[7] == le) or # middle row
-            (gr[2] == le and gr[5] == le and gr[8] == le) or # bottom row
-            (gr[2] == le and gr[4] == le and gr[6] == le) or # top right to bottom left diagonally
-            (gr[8] == le and gr[7] == le and gr[6] == le) or # up the right side
-            (gr[3] == le and gr[4] == le and gr[5] == le))   # up the middle
--- window_handler.py
+++ window_handler.py
@@ -105,3 +105,17 @@ def scorekeeper(winner):
 
     if winner == plyrs_le:
         plyrs_score += 1
+
+# checks for a winner
+def is_winner(gr, le):
+    # gr == grid and le == letter, returns True if there is a winner
+
+    return ((gr[0] == le and gr[1] == le and gr[2] == le) or # up the left side
+            (gr[0] == le and gr[4] == le and gr[8] == le) or # top left to bottom right diagonally
+            (gr[0] == le and gr[3] == le and gr[6] == le) or # top row
+            (gr[1] == le and gr[4] == le and gr[7] == le) or # middle row
+            (gr[2] == le and gr[5] == le and gr[8] == le) or # bottom row
+            (gr[2] == le and gr[4] == le and gr[6] == le) or # top right to bottom left diagonally
+            (gr[8] == le and gr[7] == le and gr[6] == le) or # up the right side
+            (gr[3] == le and gr[4] == le and gr[5] == le))   # up the middle
+
