1ce8f13073992ed2e62edffb89acc74a098f03e6
Jose Soto <luissotojose@gmail.com>
2013-06-28 13:57:34 -0400

    add import, change constant, add score text, add mouseclicks

--- window_handler.py
+++ window_handler.py
@@ -1,6 +1,7 @@
 ##import the variables
 import TTT
 import pygame
+import Plyr_Comp_options
 
 # TTT border
 left_edge = 50
@@ -17,13 +18,15 @@ first_col = 200
 second_col = 375
 
 # gridsquare constant
-gridsquare_constant = [60, 180]
+gridsquare_constant = [60, 20]
 
-
+# game pieces
+players_le = "O"
+computers_le = "X"
 
 def draw_handler(canvas, white_color, newgamefont, XorO_color, XorO_font, currentgrid):
 
-    # clear canvas -- create the grid
+    # black canvas -- create the grid
     canvas.fill((0, 0, 0))
     
     # Horizontal lines
@@ -32,26 +35,35 @@ def draw_handler(canvas, white_color, newgamefont, XorO_color, XorO_font, curren
     # Vertical lines
     pygame.draw.line(canvas, white_color, (first_col, top_edge), (first_col, bottom_edge), 12)
     pygame.draw.line(canvas, white_color, (second_col, top_edge), (second_col, bottom_edge), 12)
+
     # Text displaying score and reset option
     newgametext = newgamefont.render("N = New Game", True, white_color)
-    canvas.blit(newgametext, (8, 40))
+    computer_score = newgamefont.render("Computer: " + str(TTT.comp_score), True, white_color)
+    player_score = newgamefont.render("Player: " + str(TTT.player_score), True, white_color)
+
+    canvas.blit(computer_score, (8, 0))
+    canvas.blit(player_score, (8, 25))
+    canvas.blit(newgametext, (8, 480))
 
     # draw symbols
     for gridsquare in range(len(currentgrid)):
         XorO = XorO_font.render(currentgrid[gridsquare], True, XorO_color)
-        canvas.blit(XorO,(gridsquare_constant[0] + (160 * (gridsquare // 3)),
-                          gridsquare_constant[1] + (140 * (gridsquare % 3))))
+        canvas.blit(XorO,(gridsquare_constant[0] + (160 * (gridsquare // 3) ),
+                          gridsquare_constant[1] + (140 * (gridsquare % 3 ) )))
 
     # update the display
     pygame.display.update()
-    
-##    canvas.draw_text("Computers wins: ", (20, 25), 32, "White")
-##    canvas.draw_text("Players wins: " , (380, 25), 32, "White")
-##
 
+# keydown handler -- 
 def kd_handler(key):
     if pygame.K_n == key:
         TTT.newgame()
+        print("hit")
     
-###def mc_handler(pos):
-##    
+def mc_handler(pos, players_turn, computers_turn, currentgrid):
+
+    if pos[0] < first_col and pos[0] > left_edge and players_turn:
+        if pos[1] < first_row and pos[1] > top_edge and Plyr_Comp_options.isSpaceFree(currentgrid, 0):
+            currentgrid[0] = players_le
+            computers_turn = True
+
