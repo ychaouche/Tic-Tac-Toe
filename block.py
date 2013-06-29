94f8ae1917c838a396bff4f35927459f5c76877a
Jose Soto <luissotojose@gmail.com>
2013-06-29 00:56:17 -0400

    Add block function with doc test

---
+++ block.py
@@ -0,0 +1,21 @@
+import window_handler, possible_moves
+
+def block(grid):
+    """(list) -> int
+
+    return the index of the possible win if the opponent places his piece there
+
+    >>> block(["O", "", "", "O", "", "", "", "", ""])
+    6
+    """
+    
+    for square in range(9):
+        grid_copy = list(grid)
+        if possible_moves.isSpaceFree(grid_copy, square):
+            possible_moves.makeMove(grid_copy, window_handler.O, square)
+            if window_handler.is_winner(grid_copy, window_handler.O):
+                return square
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
