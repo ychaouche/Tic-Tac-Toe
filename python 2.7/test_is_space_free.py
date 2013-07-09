52a69b2ff666f1562ecfc89e25248e7b57a2a55b
Jose Soto <luissotojose@gmail.com>
2013-07-09 19:38:22 -0400

    Add test_is_space_free.py

---
+++ python 2.7/test_is_space_free.py
@@ -0,0 +1,16 @@
+import moves
+import unittest
+
+class TestIsSpaceFree(unittest.TestCase):
+    """ Test class for function moves.is_space_free """
+
+    def test_is_space_free(self):
+        """Checks if the correct bool was returned"""
+
+        board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+        actual = moves.is_space_free(board,1)
+        expected = True
+        self.assertEqual(expected, actual)
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
