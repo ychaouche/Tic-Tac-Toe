ec0fb005f7b9d7601ff8ff5e5be60e8eba21d0b1
Jose Soto <luissotojose@gmail.com>
2013-07-09 19:54:45 -0400

    Fix argument Argument renamed

--- python 2.7/moves.py
+++ python 2.7/moves.py
@@ -1,28 +1,17 @@
 import random
 
-# computer and player's grid options
-
 def is_space_free(board, move):
-    
-    # return true if the square is empty
     return board[move] == ' '
 
-def choose_random_move(board, selected_sqrs):
-    
-    # Returns a valid move from the passed list on the passed board
-    # Returns None if there is no valid move
+def choose_random_move(board, moves):
     possible_moves = []
-    for move in selected_sqrs:
+    for move in moves:
         if is_space_free(board, move):
             possible_moves.append(move)
-
     if len(possible_moves) != 0:
         return random.choice(possible_moves)
     else:
         None
 
-# helps computer make his moves
 def make_move(board, letter, move):
-   
-    # places letter on the grid
     board[move] = letter
