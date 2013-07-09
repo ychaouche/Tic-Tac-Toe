40aa55c07170ca78beb8fb0075897549c827a0d1
Jose Soto <luissotojose@gmail.com>
2013-07-09 19:43:29 -0400

    Add testcase Improve test case size

--- python 2.7/test_is_space_free.py
+++ python 2.7/test_is_space_free.py
@@ -12,5 +12,13 @@ class TestIsSpaceFree(unittest.TestCase):
         expected = True
         self.assertEqual(expected, actual)
 
+    def test_is_space_free_ex2(self):
+        """Checks if the correct bool was returned"""
+
+        board = [" ", "O", " ", " ", " ", " ", " ", " ", " "]
+        actual = moves.is_space_free(board,1)
+        expected = False
+        self.assertEqual(expected, actual)
+
 if __name__ == '__main__':
     unittest.main(exit=False)
