74deb79d53a8b07e5ef40ba38a7bb49cccd5991d
Jose Soto <luissotojose@gmail.com>
2013-07-10 19:23:49 -0400

    Remove function Function removed

--- python 2.7/moves.py
+++ python 2.7/moves.py
@@ -13,9 +13,6 @@ def choose_random_move(board, moves):
     else:
         None
 
-def make_move(board, letter, move):
-    board[move] = letter
-
 def is_winner(board, letter):
     possible_wins = [ [1,2,3], [4,5,6], [7,8,9], [7,4,1], [8,5,2], [9,6,3], [7,5,3], [9,5,1] ]
     for x in possible_wins:
