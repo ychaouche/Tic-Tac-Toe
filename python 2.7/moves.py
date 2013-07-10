15ddc0bba59b0b47cb03c607e451135dcebb5ed3
Jose Soto <luissotojose@gmail.com>
2013-07-10 17:52:08 -0400

    Improve moves.py Function is_winner refactored

--- python 2.7/moves.py
+++ python 2.7/moves.py
@@ -15,3 +15,9 @@ def choose_random_move(board, moves):
 
 def make_move(board, letter, move):
     board[move] = letter
+
+def is_winner(board, letter):
+    possible_wins = [ [1,2,3], [4,5,6], [7,8,9], [7,4,1], [8,5,2], [9,6,3], [7,5,3], [9,5,1] ]
+    for x in possible_wins:
+        if board[x[0]] == letter and board[x[1]] == letter and board[x[2]] == letter:
+            return True
