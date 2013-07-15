4cac50128c536aef591e4997634c6b732677bfcb
Jose Soto <luissotojose@gmail.com>
2013-07-14 20:22:38 -0400

    Made a lot of changes

--- python 2.7/move_maker.py
+++ python 2.7/move_maker.py
@@ -5,47 +5,69 @@ def get_computer_move(board, computer_letter):
         player_letter = 'O'
     else:
         player_letter = 'X'
-    for i in range(1,10):
-        board_copy = list(board)
-        if moves.is_space_free(board_copy, i):
-            make_move(board_copy, computer_letter, i)
-            if is_winner(board_copy, computer_letter):
-                return i  
-    for i in range(1,10):
-        board_copy = list(board)
-        if moves.is_space_free(board_copy, i):
-            make_move(board_copy, player_letter, i)
-            if is_winner(board_copy, player_letter):
-                return i
-    for i in [4, 6]:
-        trap = {1:3, 2:1, 3:1, 7:9, 8:7, 9:7}
-        for x in trap:
-            if board[i] == player_letter and board[x] == player_letter and moves.is_space_free(board, trap[x]):
-                return trap[x]
-    if moves.is_space_free(board, 5):
-        return 5
-    move = moves.choose_random_move(board, [1, 3, 7, 9])
-    if move != None:
+    if moves.is_space_free(board, [1,1]):
+        return [1,1]
+    for i in range(len(board)):
+        for j in range(i):
+            board_copy = [list(board[0]), list(board[1]), list(board[2])]
+            if moves.is_space_free(board_copy, [i,j]):
+                make_move(board_copy, computer_letter, [i,j])
+                if is_winner(board_copy, computer_letter):
+                    return [i,j]
+    for i in range(len(board)):
+        for j in range(i):
+            board_copy = [list(board[0]), list(board[1]), list(board[2])]
+            if moves.is_space_free(board_copy, [i,j]):
+                make_move(board_copy, player_letter, [i,j])
+                if is_winner(board_copy, player_letter):
+                    return [i,j]
+    for i in [ [1,0], [1, 2] ]:
+        if i == [1,0]:
+            counter_move = [ [0,1], [0,2], [2,1], [2,2] ]
+        else:
+            counter_move = [ [0,0], [0,1], [2,0], [2,1] ]
+        for j in counter_move:
+            if ((board[i[0]][i[1]] == player_letter) and (board[j[0]][j[1]] == player_letter) and
+                (moves.is_space_free(board, [j[0], i[1]]))):
+                return [ j[0], i[1] ]
+    move = moves.choose_random_move(board, [ [0,0], [0,2], [2,0], [2,2] ])
+    if move != None and moves.is_space_free(board, move):
         return move
-    return moves.choose_random_move(board, [2, 4, 6, 8])
+    return moves.choose_random_move(board, [ [0,1], [1,0], [1,2], [2,1] ])
 
 def is_winner(board, letter):
-    possible_wins = [ [1,2,3], [4,5,6], [7,8,9], [7,4,1], [8,5,2], [9,6,3], [7,5,3], [9,5,1] ]
-    for x in possible_wins:
-        if board[x[0]] == letter and board[x[1]] == letter and board[x[2]] == letter:
+    #Across
+    for x in board:
+        if ((x[0] == letter) and (x[1] == letter) and (x[2] == letter)):
             return True
+    #Down
+    for x in range(len(board)):
+        if ((board[0][x] == letter) and (board[1][x] == letter) and (board[2][x] == letter)):
+            return True
+    #Diagonal
+    return ((board[0][0] == letter and board[1][1] == letter and board[2][2] == letter) or
+            (board[0][2] == letter and board[1][1] == letter and board[2][0] == letter))
 
 def make_move(board, letter, move):
-    board[move] = letter
+    board[move[0]][move[1]] = letter
 
 def get_player_move(board):
     move = ' '
-    while move not in '1 2 3 4 5 6 7 8 9'.split() or not moves.is_space_free(board, int(move)):
+    while move not in '1 2 3 4 5 6 7 8 9'.split() and moves.is_space_free(board, int(move)):
         move = raw_input('What is your next move? (1-9)\n')
-    return int(move)
+        if 0 < int(move) <= 3:
+            move = [0,(int(move))-1]
+        elif 3 < int(move) <= 6:
+            move = [1,(int(move))-4]
+        elif 6 < int(move) <= 9:
+            move = [2,(int(move))-7]
+        elif ((int(move) > 9) or (int(move) < 1)):
+            move = ' '
+        return move
 
 def is_game_board_full(board):
-    for i in range(1, 10):
-        if moves.is_space_free(board, i):
-            return False
+    for i in range(len(board)):
+        for j in range(3):
+            if moves.is_space_free(board, [i,j]):
+                return False
     return True
