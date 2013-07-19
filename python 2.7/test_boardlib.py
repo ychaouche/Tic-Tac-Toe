bcfbc9049be892f7a4e5ff83499b9e631d8f5739
Jose Soto <luissotojose@gmail.com>
2013-07-19 10:22:49 -0400

    Improve tests

--- python 2.7/test_boardlib.py
+++ python 2.7/test_boardlib.py
@@ -8,7 +8,39 @@ class TestBoardLib(unittest.TestCase):
         """  A Board object's row/column arguments must be equal in length. """
 
         b = boardlib.BoardLib(3)
-        self.assertEquals(max(b.board)[0],max(b.board)[1])
+        self.assertEqual(max(b.board)[0],max(b.board)[1])
+
+    def test_board_ex2(self):
+        """ tests is_space_free """
+
+        b = boardlib.BoardLib(3)
+        b.board[(0,2)] = "X"
+        actual = b.is_space_free((0,2))
+        expected = False
+        self.assertEqual(expected, actual)
+
+    def test_board_ex3(self):
+        """ tests make_move """
+
+        b = boardlib.BoardLib(3)
+        b.board[(0,2)] = "X"
+        c = boardlib.BoardLib(3)
+        c.make_move('X', (0,2))
+        actual = c.board[(0,2)]
+        expected = b.board[(0,2)]
+        self.assertEqual(expected, actual)
+
+    def test_board_ex4(self):
+        """ tests make_move """
+
+        b = boardlib.BoardLib(3)
+        for w in b.board:
+            b.board[w] = "X"
+        actual = b.is_game_board_full()
+        expected = True
+        self.assertEqual(expected, actual)
+
+    
         
 if __name__ == '__main__':
     unittest.main(exit=False)
