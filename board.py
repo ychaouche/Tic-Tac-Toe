d6e64bc9ec10778076b1ed1fc9bc8401e3eb42a4
Yassine Chaouche <yacinechaouche@yahoo.com>
2013-07-22 14:46:02 +0100

    Added sktech to document Board.square_to_move and Board.move_to_square

--- board.py
+++ board.py
@@ -5,9 +5,9 @@ import copy
 class Board:
     def __init__(self,game, size):
         self.game   = game
-        self.size   = size        
+        self.size   = size
         self._board = {(x,y) : (' ') for x in range(size) for y in range(size)}
-        
+
     def put_mark(self, letter, move=None,square=None):
         if square :
             move = self.square_to_move(square)
@@ -15,13 +15,13 @@ class Board:
             square = self.move_to_square(move)
         #print "[DEBUG] put_mark %s on square %s (%s)" % (letter,square,move)
         self._board[move] = letter
- 
+
     def is_full(self):
         for i in sorted(self._board):
             if ' ' in self._board[i]:
                 return False
         return True
- 
+
     def is_free(self, move):
         return self._board[move] == ' '
 
@@ -30,7 +30,7 @@ class Board:
             line = []
             for col in xrange(self.size):
                 line += self._board[(row-1,col)]
-            print "     |     |     "            
+            print "     |     |     "
             print "  %s  |  %s  |  %s  " % (line[0],line[1],line[2])
             print "     |     |     "
             if row-1 != 0:
@@ -38,36 +38,67 @@ class Board:
 
     def get(self,position):
         return self._board[position]
-    
+
     def get_owner(self,position):
         mark = self.get(position)
-        if mark == self.game.human_player.playing_letter : 
+        if mark == self.game.human_player.playing_letter :
             return self.game.human_player
         elif mark == self.game.ai_player :
             return self.game.ai_player
-    
+
     def get_random_valid_move(self,moves):
         possible_moves = [move for move in moves if self.is_free(move)]
         return possible_moves and random.choice(possible_moves) or None
-        
+
     def square_to_move(self,square):
+        """
+       +-------+-------+-------+     +-------+-------+-------+     
+       |       |       |       |     |       |       |       |     
+       |   7   |   8   |   9   |     | (2,0) | (2,1) | (2,2) |     
+       |       |       |       |     |       |       |       |     
+       +-------+-------+-------+     +-------+-------+-------+     
+       |       |       |       |     |       |       |       |     
+       |   4   |   5   |   6   | --> | (1,0) | (1,1) | (1,2) |     
+       |       |       |       |     |       |       |       |     
+       +-------+-------+-------+     +-------+-------+-------+     
+       |       |       |       |     |       |       |       |     
+       |   1   |   2   |   3   |     | (0,0) | (0,1) | (0,2) |     
+       |       |       |       |     |       |       |       |     
+       +-------+-------+-------+     +-------+-------+-------+     
+        """
+
         if not 1 <= square <= self.size*self.size :
             #print "[DEBUG] square_to_move(%s) is out of range" % (square)
-            return 
+            return
         y = (square - 1) % self.size
-        x = (square - 1) /  self.size
+        x = (square - 1) / self.size
         return (x,y)
-    
+
     def move_to_square(self,move):
+        """
+        +-------+-------+-------+     +-------+-------+-------+
+        |       |       |       |     |       |       |       |
+        | (2,0) | (2,1) | (2,2) |     |   7   |   8   |   9   |
+        |       |       |       |     |       |       |       |
+        +-------+-------+-------+     +-------+-------+-------+
+        |       |       |       |     |       |       |       |
+        | (1,0) | (1,1) | (1,2) | --> |   4   |   5   |   6   |
+        |       |       |       |     |       |       |       |
+        +-------+-------+-------+     +-------+-------+-------+
+        |       |       |       |     |       |       |       |
+        | (0,0) | (0,1) | (0,2) |     |   1   |   2   |   3   |
+        |       |       |       |     |       |       |       |
+        +-------+-------+-------+     +-------+-------+-------+
+        """        
         if not move :
-            return 
+            return
         x,y = move
-        if 0 <= x <= self.size and 0 <= y <= self.size : 
+        if 0 <= x <= self.size and 0 <= y <= self.size :
             return (x*self.size) + y + 1
 
     def is_winning(self,mark):
         #Across
-        
+
         for x in range((max(self._board)[0]+1)):
             if ((self._board[(x,0)] == mark) and (self._board[(x,1)] == mark) and (self._board[(x,2)] == mark)):
                 #print "[DEBUG] HORIZONTAL",mark
@@ -86,7 +117,7 @@ class Board:
     def get_winning_move(self, mark):
         #print "[DEBUG] get_winning_move for mark",mark
         backup_board = copy.deepcopy(self._board)
-        move         = None        
+        move         = None
         for i in sorted(backup_board):
             #print "[DEBUG] board before restoring backup"
             #self.show()
@@ -101,7 +132,7 @@ class Board:
             self.put_mark(mark, i)
             if self.is_winning(mark):
                 #print '[DEBUG] square %s is a winning move' % (self.move_to_square(i))
-                move = i 
+                move = i
                 break
 
         self._board = backup_board
